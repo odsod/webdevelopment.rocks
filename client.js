e26c0ce66ef372be921c2dd10d4f9f3d976c3cdf
Oscar Söderlund <poscar@spotify.com>
2014-10-15 07:51:15 -0400

    Make it better

--- client.js
+++ client.js
@@ -1,1 +1,24 @@
+var input = document.getElementById('input');
+var list = document.getElementById('list');
+
+var todos = [
+  'Write code...',
+  'Write more code...',
+  'Go fishing',
+];
+
+var addListItem = function(todo) {
+  var listItem = document.createElement('li');
+  listItem.innerText = todo;
+  list.appendChild(listItem);
+};
+
+todos.forEach(addListItem);
+
+input.addEventListener('keydown', function(e) {
+  if (e.keyCode === 13) {
+    addListItem(input.value);
+  }
+});
+
 console.log('yo');
